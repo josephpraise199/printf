970b976d30819fbc762975f5eb27e95728a6c38c
josephpraise199 <yasafhalal@gmail.com>
2023-10-17 13:16:45 -0700

    function3

---
+++ function.d
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*get_func - produces output according to format
+*
+*@format: string to be printed
+*
+*Return: number of charaters printed else 0
+*/
+int (*get_func(const char format))(va_list)
+{
+	selector(array[]) = {
+		{'d', print_int},
+		};
+
+	int i;
+
+	for (i = 0; array[i].ident != '\0'; i++)
+	{
+		if (array[i].ident == format)
+		{
+			return (array[i].f);
+		}
+	}
+	return (0);
+}
